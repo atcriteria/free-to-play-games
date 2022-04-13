ad34de1aa2dd4d5371b6558b24eddd5b68204702
dude lao <brycelipscomb@gmail.com>
2022-04-13 03:25:49 -0400

    some more position tweaks for the GamesCards before I move on to other UI

--- src/components/Games/Card/GamesCard.js
+++ src/components/Games/Card/GamesCard.js
@@ -4,8 +4,10 @@ export default function GamesCard({data}){
     return(
         <div className='game-card'>
             <div className='game-card-descriptors'>
-                <h1>{data.title}</h1>
-                <h2>{data.developer}</h2>
+                <div className='game-card-descriptors-titles'>
+                    <h1>{data.title}</h1>
+                    <h2>{data.developer}</h2>
+                </div>
                 <p>{data.short_description}</p>
             </div>
             <img src={data.thumbnail} alt={`${data.title} Thumbnail.`}/>
