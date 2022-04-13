b09fa2921cc758eb4a0b0d76f5eaef279f1035fc
dude lao <brycelipscomb@gmail.com>
2022-04-13 03:16:54 -0400

    better styline for rendering the game cards, added the text info as a fun little overlay effect when you hover the cards so the info isn't cluttering the screen

--- src/components/Games/Card/GamesCard.js
+++ src/components/Games/Card/GamesCard.js
@@ -3,10 +3,12 @@ import './GamesCard.css';
 export default function GamesCard({data}){
     return(
         <div className='game-card'>
-            <h1>Title: {data.title}</h1>
-            <h2>Developer: {data.developer}</h2>
+            <div className='game-card-descriptors'>
+                <h1>{data.title}</h1>
+                <h2>{data.developer}</h2>
+                <p>{data.short_description}</p>
+            </div>
             <img src={data.thumbnail} alt={`${data.title} Thumbnail.`}/>
-            <p>{data.short_description}</p>
         </div>
     )
 }
