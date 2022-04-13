cc43ecfa7d7394b616f39b23446aa3bb8db04b95
dude lao <brycelipscomb@gmail.com>
2022-04-13 02:45:40 -0400

    GamesContainer and axios request to consume the API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
+import GamesContainer from './components/Games/Container/GamesContainer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Nav />
+      <GamesContainer />
     </div>
   );
 }
---
+++ src/components/Games/Container/GamesContainer.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const options = {
+    method: 'get',
+    url: 'https://www.freetogame.com/api/games',
+    params: {
+        platform: "pc",
+        category: "shooter"
+    },
+    headers: {
+        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com',
+        'X-RapidAPI-Key': process.env.REACT_APP_API_KEY
+    }
+}
+
+export default function GamesContainer(){
+    const [state, setState] = useState([])
+    useEffect(() =>{
+        
+        axios.get("https://free-to-play-games-database.p.rapidapi.com/api/games",options)
+            .then(res => {
+                setState(res.data)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
+    return(
+        <div>
+            Games get loaded here w/ axios
+            {console.log(state)}
+        </div>
+    )
+}
